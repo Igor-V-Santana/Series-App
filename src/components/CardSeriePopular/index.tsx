7f9e242e9705c149e4b665f5b26a4801a3312a72
Igor-V-Santana <igor.santanahkd@gmail.com>
2022-05-21 04:17:58 -0300

    adicionando input no header

--- src/components/CardSeriePopular/index.tsx
+++ src/components/CardSeriePopular/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './card.css';
 import { seriePopularProps } from './CardSeriePopularType';
 
 export const CardSeriePopular = ({seriePopular} : seriePopularProps) => {
+    const [favoritos, setFavoritos] = useState<any>([])
+
+    const favoritar = (serie: any) => {
+        if (favoritos.includes(seriePopular.name)){
+            desfavoritar(serie)
+        } else {
+            setFavoritos([...favoritos, serie])
+            console.log(favoritos)
+        }
+    }
+
+    const desfavoritar = (serie: any) => {
+        setFavoritos(favoritos.filter((s : any) => s !== serie))
+    }
+
     return(
-        <Link to={`/${seriePopular.id}`} className="card">
+        <div className="card">
             <img src={`https://www.themoviedb.org/t/p/original/${seriePopular.poster_path}`} alt="seriePopularPoster" />
             <div className="cardInfo">
                 <p>{seriePopular.name}</p>
                 <p>{seriePopular.first_air_date}</p>
+                {favoritos.includes(seriePopular.name) ? <button onClick={() => desfavoritar(seriePopular.name)}>desfavoritar</button> : <button onClick={() => favoritar(seriePopular.name)}>favoritar</button>}
             </div>
-        </Link>
+        </div>
     )
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,7 +5,10 @@ export const Header = () => {
         <div className='containerHeader'>
             <header />
             <div className='header'>
-                
+                <form>
+                    <input type="text" />
+                    <button>Procurar</button>
+                </form>
             </div>
         </div>
     )
