c038c5c3590c6c30e405f56aee46225b4f90b411
Igor-V-Santana <igor.santanahkd@gmail.com>
2022-05-24 00:46:52 -0300

    implementa função para formatar datas

--- src/components/CardSerieLancamento/index.tsx
+++ src/components/CardSerieLancamento/index.tsx
@@ -4,12 +4,14 @@ import { serieLancamentoProps } from "./CardSerieLancamentoType"
 
 export const CardSerieLancamento = ({serieLancamento} : serieLancamentoProps) => {
 
+    const date = new Date(serieLancamento.first_air_date).toLocaleDateString('pt-br');
+
     return(
         <Link to={`/${serieLancamento.id}`}className="card">
             <img src={`https://www.themoviedb.org/t/p/original/${serieLancamento.poster_path}`} alt="serieLancamentoPoster" />
             <div className="cardInfo">
                 <p>{serieLancamento.name}</p>
-                <p>{serieLancamento.first_air_date}</p>
+                <p>{date}</p>
             </div>
         </Link>
     )
--- src/components/CardSeriePopular/index.tsx
+++ src/components/CardSeriePopular/index.tsx
@@ -3,12 +3,15 @@ import './card.css';
 import { seriePopularProps } from './CardSeriePopularType';
 
 export const CardSeriePopular = ({seriePopular} : seriePopularProps) => {
+    
+    const date = new Date(seriePopular.first_air_date).toLocaleDateString('pt-br');
+
     return(
         <Link to={`/${seriePopular.id}`} className="card">
             <img src={`https://www.themoviedb.org/t/p/original/${seriePopular.poster_path}`} alt="seriePopularPoster" />
             <div className="cardInfo">
                 <p>{seriePopular.name}</p>
-                <p>{seriePopular.first_air_date}</p>
+                <p>{date}</p>
             </div>
         </Link>
     )
